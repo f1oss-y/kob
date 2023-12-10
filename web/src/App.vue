86079eea37e68305bef1ba22501537dca8236785
f1oss-y <3040349844@qq.com>
2023-12-11 04:08:03 +0800

    注册与登录模块

--- web/src/App.vue
+++ web/src/App.vue
@@ -11,6 +11,52 @@ import "bootstrap/dist/js/bootstrap"
 export default{
   components:{
     NavBar
+  },
+  setup(){
+/*    
+    $.ajax({
+      url:"http://127.0.0.1:3000/user/account/token/",
+      type:"post",
+      data:{
+        username:"a",
+        password:"pa",
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    });
+    $.ajax({
+      url:"http://127.0.0.1:3000/user/account/info/",
+      type:"get",
+      headers:{
+        Authorization:"Bearer "+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJhNGMxNzg3MTVhNWU0MmRkYTEyOWY0M2M2MGRiZjNlOCIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTcwMjAzOTI1NiwiZXhwIjoxNzAzMjQ4ODU2fQ.TLRMc4kUzQ6rkv54kbGLL9rdpVELV9dVYbVxYqJbha4",
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    });
+    $.ajax({
+      url:"http://127.0.0.1:3000/user/account/register/",
+      type:"post",
+      data:{
+        username:"a",
+        password:"pa",
+        confirmedPassword:"pa",
+      },
+      success(resp){
+        console.log(resp);
+      },
+      error(resp){
+        console.log(resp);
+      }
+    })
+*/
   }
 }
 </script>
