30d5227b9ce4d3b742c9187543b48f1a68bd7f07
f1oss-y <3040349844@qq.com>
2023-12-30 07:02:50 +0800

    bot代码执行_2.x

--- backendcloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backendcloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -32,7 +32,7 @@ public class WebSocketServer {
     public static RecordMapper recordMapper;
     private  static BotMapper botMapper;
     public static RestTemplate restTemplate;
-    private Game game = null;
+    public Game game = null;
     private final static String addPlayerUrl = "http://127.0.0.1:3001/player/add/";
     private final static String removePlayerurl = "http://127.0.0.1:3001/player/remove/";
 
